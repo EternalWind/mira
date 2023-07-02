b07fe7b6443378beab04020106a8641994560d4c
EternalWind <eternalwind@live.cn>
2023-07-02 10:11:15 +0000

    deploy to gae

---
+++ app/src/main/java/io/eternalwind/mira/app/controllers/TestController.java
@@ -0,0 +1,12 @@
+package io.eternalwind.mira.app.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping()
+    public String index() {
+        return "hello!";
+    }
+}
